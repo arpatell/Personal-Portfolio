6fbc51b04ff8b6856cee4981637d25f0c3e7bdba
arpatell <apatell@ad.unc.edu>
2023-09-25 09:55:27 -0400

    removing favicon

--- app/components/Links.tsx
+++ app/components/Links.tsx
@@ -4,7 +4,7 @@ const Links = () => {
     <div className="p-8">
         <h2 className="text-3xl font-semibold mb-4">Contact Me!</h2>
         <div className="container mx-auto text-center">
-            <p className='font semibold'>Thank you for checking out my portfolio. Be sure to connect with mne on LinkedIn and message me with any questions through email.</p>
+            <p className="font-semibold">Thank you for checking out my portfolio. Be sure to connect with me on LinkedIn and message me with any questions through email.</p>
             <br></br>
             <a href="https://linkedin.com/in/aaronpatel541" target="_blank" rel="noopener noreferrer" className="btn btn-active btn-ghost mr-4">LinkedIn</a>
             <a href="https://github.com/arpatell" target="_blank" rel="noopener noreferrer" className="btn btn-active btn-ghost mr-4">GitHub</a>
