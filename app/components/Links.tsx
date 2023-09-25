564355bc67656f539519e249db16f0b133dfd766
arpatell <apatell@ad.unc.edu>
2023-09-25 09:41:05 -0400

    modified page contents

--- app/components/Links.tsx
+++ app/components/Links.tsx
@@ -4,6 +4,7 @@ const Links = () => {
     <div className="p-8">
         <h2 className="text-3xl font-semibold mb-4">Contact Me!</h2>
         <div className="container mx-auto text-center">
+            <p className='font semibold'>Thank you for checking out my portfolio. Be sure to connect with mne on LinkedIn and message me with any questions through email.</p>
             <a href="https://linkedin.com/in/aaronpatel541" target="_blank" rel="noopener noreferrer" className="btn btn-active btn-ghost mr-4">LinkedIn</a>
             <a href="https://github.com/arpatell" target="_blank" rel="noopener noreferrer" className="btn btn-active btn-ghost mr-4">GitHub</a>
             <a href="mailto:aaronpatel970@gmail.com" className="btn btn-active btn-ghost mr-4">Email</a>
